d85b20c97b722e33fa5b931c40a02d6c6816eea1
Reccolino <ercole.rutolo@student.univaq.it>
2025-05-08 11:48:04 +0200

    try-except e altre piccole modifiche

--- BozzaArchitetturaManuale/validation.py
+++ BozzaArchitetturaManuale/validation.py
@@ -33,13 +33,15 @@ commons_projects = [
     "validator",
 ]
 
-'''
+
 def clone_progetti_Git():
     for project in commons_projects:
         url = f"{BASE_URL}/{ORG_URL}/{TOPIC_URL}-{project}.git"
         path = f"./cloned_repos/{project}"
         print(f"Clonando {url}")
-        Repo.clone_from(url, path, single_branch=True)'''
+        Repo.clone_from(url, path)
+        #repo = Repo.clone_from(url, path, recurse_submodules=True)  #!!!!!!
+        #repo.submodule_update(recursive=True, init=True)  #!!!!!
 #QUESTO è UN CLONE MANUALE, POI ANDRA FATTO IN MODO DINAMICO MODELLANDO LA RICHIESTA A GIT TRAMITE response, header e API key
 #PROBLEMA: l'api key di github supporta al max 60 richieste all'ora
 
@@ -59,24 +61,24 @@ def creazione_progetti_Sonar(directory):
             continue
 
         param = {
-            "name": f"ProgettoApache{repository}",
-            "project": f"ProgettoApache{repository}"
+            "name": f"ProgettoApache_{repository}",
+            "project": f"ProgettoApache_{repository}"
         }
-        response = requests.post(
-            "http://localhost:9000/api/projects/create",
-            headers= HEADER,
-            params= param)
 
-        if response.status_code==200:
-            print("Progetto creato")
-            scanner_da_terminale(param, os.path.join(directory, repository))
-            #join: ./cloned_repos\codec, per esempio
+        url = "http://localhost:9000/api/projects/create"
 
-        else:
-            print("Problemi, Problemi, Problemi !!!")
-            print(response.status_code)
-            #break
+        try:
+           response = requests.post(url, headers= HEADER, params= param)
 
+           response.raise_for_status()
+           print(f"ProgettoApache_{repository} creato")
+           scanner_da_terminale(param, os.path.join(directory, repository))
+
+        except requests.exceptions.HTTPError as e:
+           print(f"Errore HTTP ({e.response.status_code}) durante la creazione di ProgettoApache_{repository}: {e}")
+
+        except requests.exceptions.RequestException as e:
+           print(f"Errore di rete o altro problema nella richiesta: {e}")
 
 
 def scanner_da_terminale(param, path):
@@ -90,12 +92,21 @@ def scanner_da_terminale(param, path):
     """
     try:
         subprocess.run([
-            "mvn.cmd", "clean", "verify", "sonar:sonar",
+            "mvn.cmd", "clean", "verify", "jacoco:report", "org.sonarsource.scanner.maven:sonar-maven-plugin:5.1.0.4751:sonar",
             f"-Dsonar.projectKey={param.get("project")}",
             f"-Dsonar.projectName={param.get("name")}",
             f"-Dsonar.host.url=http://localhost:9000",
-            f"-Dsonar.token={os.getenv("SONAR_LOCAL_API_TOKEN")}"
+            f"-Dsonar.token={os.getenv("SONAR_LOCAL_API_TOKEN")}",
+            "-Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml"
+
         ],
+
+        #"mvn.cmd", "clean", "verify", "sonar:sonar",
+         #   f"-Dsonar.projectKey={param.get("project")}",
+          #  f"-Dsonar.projectName={param.get("name")}",
+           # f"-Dsonar.host.url=http://localhost:9000",
+            #f"-Dsonar.token={os.getenv("SONAR_LOCAL_API_TOKEN")}"
+        #],
             cwd=path,
             check=True)
         print("Analisi completata")
@@ -106,19 +117,23 @@ def scanner_da_terminale(param, path):
 
 def elimina_da_Sonar(progetto_da_eliminare):
     #ORA RIMUOVO DA SONARQUBE
-    param = {
-        "project": f"ProgettoApache{progetto_da_eliminare}"
-    }
-    response = requests.post("http://localhost:9000/api/projects/delete",
-                           headers=HEADER,
-                            params=param)
 
-    print("WEEEE")
-    if response.status_code==200:
-        print(f"Progetto {progetto_da_eliminare} eliminato")
-    else:
-        print("Problemi nell'eliminazione da SonarQube")
-        print(response)
+    param = {
+        "project": f"ProgettoApache_{progetto_da_eliminare}"
+    }
+    url = "http://localhost:9000/api/projects/delete"
+
+    try:
+        response = requests.post(url, headers=HEADER, params=param)
+
+        response.raise_for_status()
+        print(f"Progetto {progetto_da_eliminare} eliminato da SonarQube")
+
+    except requests.exceptions.HTTPError as e:
+        print(f"Errore HTTP ({e.response.status_code}) durante l'eliminazione da SonarQube: {e}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Errore di rete o altro problema nella richiesta: {e}")
 
 
 
@@ -130,6 +145,7 @@ def remove_readonly(func, path, _):
 
 def elimina_da_locale(progetto_da_eliminare):
     path = f"{DIRECTORY}/{progetto_da_eliminare}"
+
     try:
         shutil.rmtree(path, onerror=remove_readonly)
         print(f"Directory rimossa: {path}")
@@ -158,19 +174,31 @@ def risultati(directory):
         # "http://localhost:9000/api/qualitygates/project_status?projectKey=progetto-java"  ULR per vedere se il progetto passa il Quality Gate
 
         param={
-            "component": f"ProgettoApache{project}",
+            "component": f"ProgettoApache_{project}",
             "metricKeys": "bugs,vulnerabilities,code_smells,coverage,duplicated_lines_density"
         }
 
         #al posto di fare due chiamate (una per vedere se il progetto è presente su Sonar e una per mostrare le metriche dei progetti)
         #ne faccio una sola che fa entrambe le funzioni: uso la chiamata delle metriche --> se mi restituisce 4040 significa che
         #il progetto non è presente su Sonar e, quindi, va eliminato
-        response = requests.get(f"http://localhost:9000/api/measures/component",
-                                headers=HEADER,
-                                params=param)
 
-        if response.status_code==200:
+        url = "http://localhost:9000/api/measures/component"
+
+        try:
+            response = requests.get(url, headers=HEADER, params=param)
+
+            #response.raise_for_status()
+            #in questo caso 404 non mi deve generare l'eccezione: voglio esplicitamente controllare
+            #se la response sia 404 per eliminare il progetto
+
+            if response.status_code == 404:  # se il progetto è mancante la chiamata restituirà 404
+                print("Progetto mancante in SonarQube  --> allora va eliminato in locale")
+                elimina_da_locale(project)
+                return
+
             print(f"L'analisi di {project} ha restituito: {response.json()}")
+
+                        #TOD: SALVARE METRICHE PER OGNI PROGETTO IN UN FILE .csv
 
 
             #se il progetto è su SonarQube, ma lo scanner aveva dato problemi, allora quel progetto è rimasto su Sonar
@@ -182,20 +210,15 @@ def risultati(directory):
                 elimina_da_locale(project)
 
 
-                #TOD: QUI VA FATTO L'IF CON LA SOGLIA MASSIMA DELLE METRICHE DEI PROGETTI ---> DA FARE PIU AVANTI
+                        #TOD: QUI VA FATTO L'IF (MINING) CON LA SOGLIA MASSIMA DELLE METRICHE DEI PROGETTI ---> DA FARE PIU AVANTI
 
-
-        elif (response.status_code == 404):  # se il progetto è mancante la chiamat restituirà 404
-            print("Progetto mancante in SonarQube  --> allora eliminalo in locale")
-            elimina_da_locale(project)
-
-        else:
-            print("Altro problema")
-            print(response)
+        except requests.exceptions.RequestException as e:
+             print(f"Errore di rete o altro problema nella richiesta: {e}")
 
 
 
-#clone_progetti_Git()
-#creazione_progetti_Sonar(DIRECTORY)
+
+clone_progetti_Git()
+creazione_progetti_Sonar(DIRECTORY)
 risultati(DIRECTORY)
 
